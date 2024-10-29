1cdc8c0c8615ae646ef50b534f11874f63d5f9b0
acreciti <acreciti@gmail.com>
2024-10-29 13:52:28 -0500

    many changes

--- about.markdown
+++ about.markdown
@@ -4,15 +4,10 @@ title: About
 permalink: /about/
 ---
 
-This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at [jekyllrb.com](https://jekyllrb.com/)
+My name is acreciti. I'm a cybersecurity student, indie hacker, dark triad ubermensch philosopher-king.
+I post content about things I like. My X profile and others are linked below.
+I use arch, btw. 
 
-You can find the source code for Minima at GitHub:
-[jekyll][jekyll-organization] /
-[minima](https://github.com/jekyll/minima)
-
-You can find the source code for Jekyll at GitHub:
-[jekyll][jekyll-organization] /
-[jekyll](https://github.com/jekyll/jekyll)
-
-
-[jekyll-organization]: https://github.com/jekyll
+Find me on:
+- [GitHub](https://github.com/acrecit)
+- [X/Twitter](https://x.com/acreciti)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,3 @@
-// Theme switcher
-const initTheme = () => {
-    const themeToggle = document.createElement('button');
-    themeToggle.classList.add('theme-toggle');
-    themeToggle.setAttribute('aria-label', 'Toggle dark mode');
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    let isDark = localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && prefersDark);
-    const setTheme = (dark) => {
-        document.documentElement.classList.toggle('dark', dark);
-        localStorage.setItem('theme', dark ? 'dark' : 'light');
-        isDark = dark;
-    };
-    themeToggle.addEventListener('click', () => setTheme(!isDark));
-    setTheme(isDark);
-    document.querySelector('.nav-links')?.appendChild(themeToggle);
-};
 // Smooth scroll for anchor links
 const initSmoothScroll = () => {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -24,8 +8,8 @@ const initSmoothScroll = () => {
         });
     });
 };
+
 // Initialize
 document.addEventListener('DOMContentLoaded', () => {
-    initTheme();
     initSmoothScroll();
 });
